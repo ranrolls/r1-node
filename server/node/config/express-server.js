d929ab4dd73b4ba5470e7773e5560751982bdb76
RANJAN <roybellnet@gmail.com>
2018-08-15 23:38:41 +0530

    0.0.1-0 Add reload support to nodeServer nodemon by browser-sync

--- server/node/config/express-server.js
+++ server/node/config/express-server.js
@@ -1,11 +1,9 @@
 const express = require('express'); const path = require('path');
+const browserSync = require('browser-sync');
 const nodeVar = require('./nodeVariable');
 let app = express();
-// set global variable server on every route
-app.set('port', process.env.PORT || 3000);
-// set view engine
+app.set('port', process.env.PORT || 3010);
 app.set('view engine', nodeVar.common.viewEngine);
-// set view path
 app.set('views', nodeVar.common.viewPath);
 // set variable which need to passed to view
 app.locals.siteTitle = "r1-node page title";
@@ -17,7 +15,7 @@ app.use(require(nodeVar.modules.login.routePath));
 app.use(require(nodeVar.modules.crudFormFile.routePath));
 app.use(require(nodeVar.modules.crudFormDb.routePath));
 app.use(require(nodeVar.modules.test.routePath));
-// create server
-const server = app.listen(app.get('port'),
-    () => console.log("Express core server")
-);
+const server = app.listen(app.get('port'), () => {
+    browserSync({        open: false,        port: app.get('port') + 1,
+        proxy: 'localhost:' + app.get('port'),        ui: false    });
+});
